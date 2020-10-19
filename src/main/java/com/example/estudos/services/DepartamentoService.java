e8a8b61f6b4b38fe5db408c79613b6e92e070f0e
marcsue <marcsuejr@gmail.com>
2020-10-19 14:08:48 -0300

    logs e try cacthc

--- src/main/java/com/example/estudos/services/DepartamentoService.java
+++ src/main/java/com/example/estudos/services/DepartamentoService.java
@@ -7,40 +7,86 @@ import org.springframework.stereotype.Service;
 
 import com.example.estudos.dao.DepartamentoRepository;
 import com.example.estudos.dataModel.Departamento;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class DepartamentoService 
 {
+	private final Logger log = LoggerFactory.getLogger(DepartamentoService.class);
+	
 	@Autowired 
 	private DepartamentoRepository departamentoRepository;
 	
 	public Iterable<Departamento> findAll()
 	{
-		return departamentoRepository.findAll();
+		try
+		{
+			return departamentoRepository.findAll();
+		}
+		catch (Exception e) 
+		{
+			log.error("erro ao buscar todos os departamentos",e.getMessage());
+			return null;
+		}
 	}
 	
 	public Optional<Departamento> findById(Integer id)
 	{
-		return departamentoRepository.findById(id);
+		try 
+		{
+			return departamentoRepository.findById(id);
+		} 
+		catch (Exception e) 
+		{
+			log.error("erro ao buscar o departamento por ID",e.getMessage());
+			return null;
+		}
+	
 	}
 	
 	public Departamento insertDepartamento(Departamento departamento)
 	{
-		return departamentoRepository.save(departamento);
+		try 
+		{
+			return departamentoRepository.save(departamento);
+		} 
+		catch (Exception e) 
+		{
+			log.error("erro ao inserir departamento",e.getMessage());
+			return null;
+		}
+		
 	}
 	
 	public Iterable<Departamento> departamentosContainsT ()
 	{
-		return departamentoRepository.departamentosContainsT();
+		try
+		{
+			return departamentoRepository.departamentosContainsT();
+		}
+		catch (Exception e) 
+		{
+			log.error("erro ao buscar departamentos que contaim T",e.getMessage());
+			return null;
+		}
 	}
 	
 	public Boolean deleteDepartamento (Departamento departamento)
 	{
-		departamentoRepository.delete(departamento);
-		if (findById(departamento.getId()) == null)
-			return true;
-		else
+		try
+		{
+			departamentoRepository.delete(departamento);
+			if (findById(departamento.getId()) == null)
+				return true;
+			else
+				return false;
+		}
+		catch (Exception e) 
+		{
+			log.error("erro ao deletar departamento",e.getMessage());
 			return false;
+		}
 	}
 }
 
